3bade025516cd06ced1fcf92b327365e1e993780
Adam Goth <adamgoth@gmail.com>
2016-11-11 07:34:10 -0700

    Initial Commit

---
+++ selfie-share/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  selfie-share
+//
+//  Created by Adam Goth on 11/11/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
